3b4f8991502bc1fc7c2a8a57458d6e0a541806b7
YevhenDenyk <denyk.yevhen@gmail.com>
2023-03-13 16:28:55 +0200

    corrected

--- services/file.services.js
+++ services/file.services.js
@@ -3,11 +3,11 @@ const path = require("path");
 
 module.exports = {
     reader: async () => {
-        const buffer = await fs.readFile(path.join(__dirname, 'dataBase', 'users.json'));
+        const buffer = await fs.readFile(path.join(process.cwd(), 'dataBase', 'users.json'));
         return JSON.parse(buffer.toString())
     },
     writer: async (users) => {
-        await fs.writeFile(path.join(__dirname, 'dataBase', 'users.json'), JSON.stringify(users))
+        await fs.writeFile(path.join(process.cwd(), 'dataBase', 'users.json'), JSON.stringify(users))
     },
 }
 
